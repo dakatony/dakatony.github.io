097779d7aa2747c8cd75d1621faa7c4cf2d5bd4b
Anthony Bonifacio Alvarez <anthonyjba@gmail.com>
2023-11-17 01:06:25 +0100

    add promo black friday

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,27 +118,37 @@ if (localStorage.getItem("theme") === "light_theme") {
 
 
 // Get the modal
-var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
+var modalPromo = document.getElementById("modalPromo")
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btnPromo = document.getElementById("btnPromo");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close")[0];
+var cerrar = document.getElementById("cerrar")
+var closePromo = document.getElementById("closePromo")
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnPromo.onclick = function() {
+ modalPromo.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+//span.onclick = function() {
+ // modal.style.display = "none";
+//}
+
+cerrar.onclick = function() {
+  modalPromo.style.display = "none";
+}
+closePromo.onclick = function() {
+  modalPromo.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+/* window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+} */
